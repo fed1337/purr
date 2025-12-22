77094fb563e7b8414f48a8bedb16d971bc27aa3f
Fedor Sedov <fedorwww@gmail.com>
2025-12-23 01:24:47 +0200

    tests: update test_verify_state_token test, merge upstream log fix

--- lemur/tests/test_oauth.py
+++ lemur/tests/test_oauth.py
@@ -1,8 +1,7 @@
 from datetime import datetime, timedelta
+
 from freezegun import freeze_time
-
 from flask import current_app
-
 from lemur.auth.views import *  # noqa
 from lemur.tests.conf import OAUTH_STATE_TOKEN_STALE_TOLERANCE_SECONDS, OAUTH_STATE_TOKEN_SECRET
 
@@ -33,11 +32,10 @@ def test_verify_state_token(client):
     token = generate_state_token()
     assert verify_state_token(token)
 
-    with freeze_time(
-        datetime.now() - timedelta(seconds=OAUTH_STATE_TOKEN_STALE_TOLERANCE_SECONDS), tick=True
-    ):
-        stale_token = generate_state_token()
-    assert not verify_state_token(stale_token)
+    with freeze_time(datetime.now() + timedelta(
+            seconds=OAUTH_STATE_TOKEN_STALE_TOLERANCE_SECONDS + 1
+    )):
+        assert not verify_state_token(token)
 
     assert not verify_state_token("123456:f4k8")
     assert not verify_state_token("123456::f4k8")
